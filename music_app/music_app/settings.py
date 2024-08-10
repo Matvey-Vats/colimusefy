616b60650f27aeea22b4a0130cd4884708c0b003
Matvey <matviyvac@gmail.com>
2024-08-10 17:59:35 +0300

    tracks app

--- music_app/music_app/settings.py
+++ music_app/music_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=gd2)4c5@gqx7j8z*a(!dceag-6_gy&noma@981xls+fm*&p6w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'tracks.apps.TracksConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'music_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +118,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = (
+    BASE_DIR / 'media',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+   'handlers': {
+       'console': {'class': 'logging.StreamHandler'}
+   },
+   'loggers': {
+       'django.db.backends': {
+           'handlers': ['console'],
+           'level': 'DEBUG',
+       }
+   }
+}
